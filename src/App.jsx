67f494af9669210dd8a8482a0cafefdf4e7b833f
Sam Cassidy <sam@cassidys.email>
2023-12-30 21:11:30 +0000

    Add headline component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Header } from './components/Header'
 
 function App() {
-
   return (
     <>
       <div className='flex justify-center'>
---
+++ src/components/Headline.jsx
@@ -0,0 +1,8 @@
+export function Headline() {
+  return (
+    <div className='flex'>
+
+    </div>
+  )
+
+}
